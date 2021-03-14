59fe55ec08371b133bae6b982695e3ebb1d72faf
Kishan Jasani <kishanjasani007@yahoo.in>
2021-03-14 15:23:05 +0530

    Fetch the favicon

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,13 @@
 import Header from "./header"
 import Footer from "./footer"
+import Head from "next/head"
 
 const Layout = ({ data, children }) => {
     return(
         <div>
+            <Head>
+                <link rel="shortcut icon" href={ data?.header?.favicon } />
+            </Head>
             <Header header={ data?.header } headerMenu={ data?.menus?.headerMenus }/>
             {children}
             <Footer footer={ data?.footer } />
